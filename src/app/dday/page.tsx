85ffe2018b66488d5d61b4f1ccfa4e194a9cac43
hyeon9782 <hyeon9782@gmail.com>
2023-05-26 02:13:03 +0900

    feat: d-day 컴포넌트 개발 중

---
+++ src/app/dday/page.tsx
@@ -0,0 +1,10 @@
+import Countdown from '@/components/Countdown'
+import React from 'react'
+
+export default function DDayPage() {
+  return (
+    <section>
+        <Countdown />
+    </section>
+  )
+}
---
+++ src/components/Countdown.tsx
@@ -0,0 +1,27 @@
+'use client'
+
+import { useState } from "react";
+
+export default function Countdown() {
+    const today = new Date()
+  
+    const [dday, setDDay] = useState();
+
+    const [title, setTitle] = useState('');
+    
+    const handleChange = e => {
+        const diff = new Date(e.target.value).getTime() - today.getTime();
+        const days = Math.floor(diff / 86400000)
+        setDDay(days);
+    }
+  return (
+    <article className="bg-gray-500 w-11/12 my-0 mx-auto text-center p-4">
+        <form className="">
+            <input type="text" value={title} onChange={e => setTitle(e.target.value)}/><br/>
+            <input type="date" onChange={handleChange}/>
+        </form>
+        <div>{title}</div>
+        <div>{dday}일 전</div>
+    </article>
+  )
+}
