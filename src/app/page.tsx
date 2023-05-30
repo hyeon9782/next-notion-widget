a66bf3a0c95d48f6ec22b04bd08b189ae901619d
hyeon9782 <hyeon9782@gmail.com>
2023-05-30 15:05:38 +0900

    docs: ignore 파일 변경

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,7 @@
-import HeatMapChart from "@/components/charts/HeatMapChart";
-
 export default function Home() {
-  const heatmapData = [
-    { x: "Category 1", y: "January", value: 10 },
-    { x: "Category 1", y: "February", value: 15 },
-    // Add more data objects as needed
-  ];
   return (
     <section>
-      <HeatMapChart data={heatmapData} />
+      <h1>메인</h1>
     </section>
   );
 }
